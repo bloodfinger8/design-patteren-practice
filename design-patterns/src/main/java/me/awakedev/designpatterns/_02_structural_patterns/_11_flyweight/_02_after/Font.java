a1afa4065db8fd903461d223982959c23c9b8b7a
awakedev <didwodn8822@gmail.com>
2022-06-27 00:35:19 +0900

    ADD - 11. 플라이웨이트 패턴 간단 구현

---
+++ design-patterns/src/main/java/me/awakedev/designpatterns/_02_structural_patterns/_11_flyweight/_02_after/Font.java
@@ -0,0 +1,10 @@
+package me.awakedev.designpatterns._02_structural_patterns._11_flyweight._02_after;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor @Getter
+public final class Font {
+    final String family;
+    final int size;
+}
