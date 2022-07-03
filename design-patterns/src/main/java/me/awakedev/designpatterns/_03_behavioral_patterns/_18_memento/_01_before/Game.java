1f1e558bd8b480c123b371ca6fb475c8b0937f9d
awakedev <didwodn8822@gmail.com>
2022-07-03 21:24:08 +0900

    ADD - 18. 메멘토 디자인 패턴 간단 구현

---
+++ design-patterns/src/main/java/me/awakedev/designpatterns/_03_behavioral_patterns/_18_memento/_01_before/Game.java
@@ -0,0 +1,22 @@
+package me.awakedev.designpatterns._03_behavioral_patterns._18_memento._01_before;
+
+public class Game {
+    private int blueTeamScore;
+    private int redTeamScore;
+
+    public int getBlueTeamScore() {
+        return blueTeamScore;
+    }
+
+    public void setBlueTeamScore(int blueTeamScore) {
+        this.blueTeamScore = blueTeamScore;
+    }
+
+    public int getRedTeamScore() {
+        return redTeamScore;
+    }
+
+    public void setRedTeamScore(int redTeamScore) {
+        this.redTeamScore = redTeamScore;
+    }
+}
---
+++ design-patterns/src/main/java/me/awakedev/designpatterns/_03_behavioral_patterns/_18_memento/_02_after/GameSave.java
@@ -0,0 +1,19 @@
+package me.awakedev.designpatterns._03_behavioral_patterns._18_memento._02_after;
+
+public class GameSave {
+    private int redTeamScore;
+    private int blueTeamScore;
+
+    public GameSave(int redTeamScore, int blueTeamScore) {
+        this.redTeamScore = redTeamScore;
+        this.blueTeamScore = blueTeamScore;
+    }
+
+    public int getRedTeamScore() {
+        return redTeamScore;
+    }
+
+    public int getBlueTeamScore() {
+        return blueTeamScore;
+    }
+}
