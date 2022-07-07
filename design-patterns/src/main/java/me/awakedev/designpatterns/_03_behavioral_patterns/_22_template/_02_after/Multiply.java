ce9174c46fd253157a65782aef61208f834817ab
awakedev <didwodn8822@gmail.com>
2022-07-07 23:06:51 +0900

    FIX -  생성자 수정

--- design-patterns/src/main/java/me/awakedev/designpatterns/_03_behavioral_patterns/_22_template/_02_after/Multiply.java
+++ design-patterns/src/main/java/me/awakedev/designpatterns/_03_behavioral_patterns/_22_template/_02_after/Multiply.java
@@ -1,10 +1,9 @@
 package me.awakedev.designpatterns._03_behavioral_patterns._22_template._02_after;
 
 public class Multiply extends FileProcessor{
-    private String path;
 
     public Multiply(String path) {
-        this.path = path;
+        super(path);
     }
 
     @Override
