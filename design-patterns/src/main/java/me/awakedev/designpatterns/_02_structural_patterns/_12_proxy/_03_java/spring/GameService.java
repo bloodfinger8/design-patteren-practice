c4040354a9bc5b38e15c2de5f07d1a7c543468fe
awakedev <didwodn8822@gmail.com>
2022-07-17 01:34:25 +0900

    ADD - 12. 프록시 디자인 패턴 자바 실사용 알아보기

---
+++ design-patterns/src/main/java/me/awakedev/designpatterns/_02_structural_patterns/_12_proxy/_03_java/spring/GameService.java
@@ -0,0 +1,10 @@
+package me.awakedev.designpatterns._02_structural_patterns._12_proxy._03_java.spring;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GameService {
+    public void startGame() {
+        System.out.println("이 자리에 오신 여러분을 진심으로 환영합니다.");
+    }
+}
