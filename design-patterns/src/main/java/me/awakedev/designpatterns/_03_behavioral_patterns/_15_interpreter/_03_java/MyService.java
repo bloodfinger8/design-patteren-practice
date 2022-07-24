e849e939a05ede930723ef6ad1da6a0389ef6684
awakedev <didwodn8822@gmail.com>
2022-07-24 23:52:36 +0900

    ADD - 15.인터프린터 디자인패턴 실사용 알아보기

---
+++ design-patterns/src/main/java/me/awakedev/designpatterns/_03_behavioral_patterns/_15_interpreter/_03_java/MyService.java
@@ -0,0 +1,15 @@
+package me.awakedev.designpatterns._03_behavioral_patterns._15_interpreter._03_java;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+
+public class MyService implements ApplicationRunner {
+    @Value("#{2 + 5}")
+    private String value;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        System.out.println(value);
+    }
+}
